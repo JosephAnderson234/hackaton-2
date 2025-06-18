b0e69f04bd95843cbcb964c27e1a0fc485ec5fdb
JosephAnderson234 <joseph.cose@utec.edu.pe>
2025-06-17 21:52:01 -0500

    feat: implemented the corresponding types and request

--- src/types/expenseType.tsx
+++ src/types/expenseType.tsx
@@ -10,3 +10,14 @@ export interface Expense {
   amount: number;
   category: Category;
 }
+
+export interface ExpenseSummary {
+  id: number;
+  expenseCategory: {
+    id: number;
+    name: string;
+  }
+  year: number;
+  month: number;
+  amount: number;
+}
--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -24,6 +24,20 @@ export const register = async (signupRequest: RegisterRequest) => {
     }
 }
 
+export const getAllExpenses = async (token: string) => {
+    try{
+        const response = await axios.get(`${BACKEND_URL}/expenses_summary`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching expenses:", error);
+        throw error;
+    }
+}
+
 export async function getExpensesByCategory(
     token: string,
     year: number,
